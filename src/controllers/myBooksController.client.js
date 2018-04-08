3056bd57c53771bde0071bf515d89625d4164196
R.C. Long <deac.long@wsu.edu>
2018-04-08 12:21:33 -0700

    Change offers logic

--- src/controllers/myBooksController.client.js
+++ src/controllers/myBooksController.client.js
@@ -1,24 +1,36 @@
 'use strict';
 
 var myBooks;
+var offers;
 var booksDiv = "";
 var offersDiv = "";
 var tradesDiv = "";
+var user;
 
 $(document).ready(function () {
+  $.get("/api/user/:id", function (d, s) {
+    user = d;
+  });
   $.when(getUserBooks()).done(function (r) {
     myBooks = r;
-    makeOffersDiv();
-    $("#o").append(offersDiv);
     makeTradesDiv();
     $("#t").append(tradesDiv);
     makeBooksDiv();
     $("#m").append(booksDiv);
   })
+  getUserOffers();
 });
 
 function getUserBooks() {
   return $.get("/api/user/:id/books");
+}
+
+function getUserOffers() {
+  $.get("/api/user/:id/offer/", function (d) {
+    offers = d;
+    makeOffersDiv();
+    $("#o").append(offersDiv);
+  });
 }
 
 function makeBooksDiv() {
@@ -64,7 +76,7 @@ function makeTradesDiv() {
 }
 
 function makeOffersDiv() {
-  myBooks.forEach((b) => {
+  offers.forEach((b) => {
     if (b.info.offerUserId !== "") {
       offersDiv += `
       <div class="col-3 border rounded" id="m-o-${b.info.isbn13}">
@@ -75,25 +87,25 @@ function makeOffersDiv() {
             Author: ${b.book.author}<br/>
             Date: ${b.book.date}<br/>
             ISBN13: ${b.book.isbn13}<br/>
-            <button type="button" class="btn btn-primary" onclick="changeOffer(true)">Accept</button>
+            <button type="button" class="btn btn-primary" onclick="changeOffer(true, '${user.id}', '${b.info.isbn13}', '${b.info.offerUserId}', '${b.info.offerIsbn13}')">Accept</button>
           </div>
           <div class="col-5">
             <img src="${b.book.img_url}"/>
           </div>
         </div>
       </div>
-      <div class="col-3 border rounded" id="m-o-${b.info.isbn13}">
+      <div class="col-3 border rounded" id="t-o-${b.info.isbn13}">
         <div class="row">
-          <h4 class="col-12 text-center">Somedude</h4>
+          <h4 class="col-12 text-center">${b.info.offerUserId}</h4>
           <div class="col-5">
-            <img src="${b.book.img_url}"/>
+            <img src="${b.offer.img_url}"/>
           </div>
           <div class="col-7">
-            Title: ${b.book.title}<br/>
-            Author: ${b.book.author}<br/>
-            Date: ${b.book.date}<br/>
-            ISBN13: ${b.book.isbn13}<br/>
-            <button type="button" class="btn btn-danger" onclick="changeOffer(false)">Reject</button>
+            Title: ${b.offer.title}<br/>
+            Author: ${b.offer.author}<br/>
+            Date: ${b.offer.date}<br/>
+            ISBN13: ${b.offer.isbn13}<br/>
+            <button type="button" class="btn btn-danger" onclick="changeOffer(false, '${user.id}', '${b.info.isbn13}', '${b.info.offerUserId}', '${b.info.offerIsbn13}')">Reject</button>
           </div>
         </div>
       </div>
@@ -102,20 +114,16 @@ function makeOffersDiv() {
   });
 }
 
-function changeOffer(accept) {
-
-  var uId = "950699122289344512";
-  var bId = "9780295741352";
-  var offerUId = "982048743275941888";
-  var offerBId = "0374706344";
-  
+function changeOffer(accept, uId, bId, offerUId, offerBId) {
   var body = {
     accept: accept,
     uId: uId,
     bId: bId,
-    tradeUId: offerUId,
-    tradeBId: offerBId,
+    offerUId: offerUId,
+    offerBId: offerBId,
   }
+
+  console.log(body);
   $.ajax({
     type: "PUT",
     url: "/api/user/:id/offer/:id",
